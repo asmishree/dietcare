661ee88c99a8fb0aa468940ae2ac208430c0fd64
asmishree <146105965+asmishree@users.noreply.github.com>
2023-11-17 12:39:59 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,11 +8,10 @@ function About() {
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
-              Pitchfork Kickstarter Taxidermy
+              Dietcare 
             </h1>
             <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">
-              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-              gentrify, subway tile poke farm-to-table.
+             An initative to propagate healthy eating and lifestyle .
             </p>
           </div>
           <div className="flex flex-wrap -m-4">
@@ -32,11 +31,10 @@ function About() {
                   </svg>
                 </div>
                 <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
-                  Shooting Stars
+                  BMI Calculator
                 </h2>
                 <p className="leading-relaxed text-base">
-                  Fingerstache flexitarian street art 8-bit waist co, subway
-                  tile poke farm.
+                The Body Mass Index (BMI) Calculator can be used to calculate BMI value and corresponding weight status while taking age into consideration.
                 </p>
               </div>
             </div>
@@ -58,11 +56,10 @@ function About() {
                   </svg>
                 </div>
                 <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
-                  The Catalyzer
+                Body Fat Calculator
                 </h2>
                 <p className="leading-relaxed text-base">
-                  Fingerstache flexitarian street art 8-bit waist co, subway
-                  tile poke farm.
+                The Body Fat Calculator can be used to estimate your total body fat based on specific measurements.
                 </p>
               </div>
             </div>
@@ -83,11 +80,10 @@ function About() {
                   </svg>
                 </div>
                 <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
-                  Neptune
+                  BMR Calculator
                 </h2>
                 <p className="leading-relaxed text-base">
-                  Fingerstache flexitarian street art 8-bit waist co, subway
-                  tile poke farm.
+                The basal metabolic rate (BMR) is the amount of energy needed while resting in a temperate environment when the digestive system is inactive.
                 </p>
               </div>
             </div>
@@ -107,11 +103,10 @@ function About() {
                   </svg>
                 </div>
                 <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
-                  Melanchole
+                 Dietplan
                 </h2>
                 <p className="leading-relaxed text-base">
-                  Fingerstache flexitarian street art 8-bit waist co, subway
-                  tile poke farm.
+                  This generates the Dietplan for the day for a user based on their Calorie requirements.
                 </p>
               </div>
             </div>
@@ -131,11 +126,10 @@ function About() {
                   </svg>
                 </div>
                 <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
-                  Bunker
+                  Blogs
                 </h2>
                 <p className="leading-relaxed text-base">
-                  Fingerstache flexitarian street art 8-bit waist co, subway
-                  tile poke farm.
+                  Informative Blogs results in enhancement of knowledge and idea for healthy eating and body.
                 </p>
               </div>
             </div>
@@ -155,11 +149,10 @@ function About() {
                   </svg>
                 </div>
                 <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
-                  Ramona Falls
+                  Security & Personalisation
                 </h2>
                 <p className="leading-relaxed text-base">
-                  Fingerstache flexitarian street art 8-bit waist co, subway
-                  tile poke farm.
+                  All these features are available with a personalized and secure environment.
                 </p>
               </div>
             </div>
