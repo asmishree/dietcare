78d16847e83fcccc3f612ebff502072ca53ba914
asmishree <146105965+asmishree@users.noreply.github.com>
2023-11-19 08:14:30 +0530

    updates

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { IoFitness } from "react-icons/io5";
+import { IoIosFitness } from "react-icons/io";
+import { MdArticle } from "react-icons/md";
+import { LuCalendarHeart } from "react-icons/lu";
 
 function About() {
   return (
@@ -8,10 +11,10 @@ function About() {
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
-              Dietcare 
+              Dietcare
             </h1>
             <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">
-             An initative to propagate healthy eating and lifestyle .
+              An initative to propagate healthy eating and lifestyle .
             </p>
           </div>
           <div className="flex flex-wrap -m-4">
@@ -34,102 +37,77 @@ function About() {
                   BMI Calculator
                 </h2>
                 <p className="leading-relaxed text-base">
-                The Body Mass Index (BMI) Calculator can be used to calculate BMI value and corresponding weight status while taking age into consideration.
+                  The Body Mass Index (BMI) Calculator is used to calculate
+                  BMI value and corresponding weight status while taking age
+                  into consideration.
                 </p>
               </div>
             </div>
             <div className="xl:w-1/3 md:w-1/2 p-4">
               <div className="border border-gray-200 p-6 rounded-lg">
                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-200 text-blue-600 mb-4">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="w-6 h-6"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle cx="6" cy="6" r="3"></circle>
-                    <circle cx="6" cy="18" r="3"></circle>
-                    <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
-                  </svg>
+                  <IoIosFitness className="w-6 h-6"               
+                  />
+                    
                 </div>
                 <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
-                Body Fat Calculator
+                  Body Fat Calculator
                 </h2>
                 <p className="leading-relaxed text-base">
-                The Body Fat Calculator can be used to estimate your total body fat based on specific measurements.
+                  The Body Fat Calculator can be used to estimate your total
+                  body fat based on specific measurements.
                 </p>
               </div>
             </div>
             <div className="xl:w-1/3 md:w-1/2 p-4">
               <div className="border border-gray-200 p-6 rounded-lg">
                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-200 text-blue-600 mb-4">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="w-6 h-6"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                    <circle cx="12" cy="7" r="4"></circle>
-                  </svg>
+                  <IoFitness className="w-6 h-6" />
                 </div>
                 <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
                   BMR Calculator
                 </h2>
                 <p className="leading-relaxed text-base">
-                The basal metabolic rate (BMR) is the amount of energy needed while resting in a temperate environment when the digestive system is inactive.
+                  The Basal metabolic rate (BMR) is the amount of energy needed
+                  while resting in a temperate environment where Digestive
+                  system is inactive.
                 </p>
               </div>
             </div>
             <div className="xl:w-1/3 md:w-1/2 p-4">
               <div className="border border-gray-200 p-6 rounded-lg">
                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-200 text-blue-600 mb-4">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="w-6 h-6"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1zM4 22v-7"></path>
-                  </svg>
+                  < LuCalendarHeart 
+                    
+                    className="w-6 h-6 "
+                    
+                  />
+            
                 </div>
                 <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
-                 Dietplan
+                  Dietplan
                 </h2>
                 <p className="leading-relaxed text-base">
-                  This generates the Dietplan for the day for a user based on their Calorie requirements.
+                  This generates the Dietplan for the day for a user based on
+                  their Calorie requirements.
                 </p>
               </div>
             </div>
             <div className="xl:w-1/3 md:w-1/2 p-4">
               <div className="border border-gray-200 p-6 rounded-lg">
                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-200 text-blue-600 mb-4">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                  <MdArticle                 
                     className="w-6 h-6"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
-                  </svg>
+                   
+                  />
+                  
                 </div>
                 <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
                   Blogs
                 </h2>
                 <p className="leading-relaxed text-base">
-                  Informative Blogs results in enhancement of knowledge and idea for healthy eating and body.
+                  Informative Blogs provides idea
+                  for healthy eating and body.
                 </p>
               </div>
             </div>
@@ -152,15 +130,17 @@ function About() {
                   Security & Personalisation
                 </h2>
                 <p className="leading-relaxed text-base">
-                  All these features are available with a personalized and secure environment.
+                  All these features are available with a personalized and
+                  secure environment.
                 </p>
               </div>
             </div>
           </div>
-         <a href="https://dietcare-admin.vercel.app" target="_blank">
-         <button className="flex mx-auto mt-16 text-white bg-blue-600 border-0 py-2 px-8 focus:outline-none hover:bg-blue-800 rounded text-lg">
-            Admin Dashboard 
-          </button></a>
+          <a href="https://dietcare-admin.vercel.app" target="_blank">
+            <button className="flex mx-auto mt-16 text-white bg-blue-600 border-0 py-2 px-8 focus:outline-none hover:bg-blue-800 rounded text-lg">
+              Admin Dashboard
+            </button>
+          </a>
         </div>
       </section>
     </div>
--- src/pages/Dietplan.jsx
+++ src/pages/Dietplan.jsx
@@ -1,69 +1,78 @@
-// BMRCalculator.js
 import React, { useState } from 'react';
+import axios from 'axios';
 import toast from 'react-hot-toast';
+import API from '../API';
 
 const Dietplan = () => {
   const [bmr, setBmr] = useState('');
-  const [activity, setActivity] = useState('mactive');
+  const [activity, setActivity] = useState('moderately');
   const [dietpref, setDietpref] = useState('veg');
   const [result, setResult] = useState(null);
 
-  const MyDietplan = () => {
-    if ( bmr=="") {
-        toast.error('Please enter all fields.');
-        return;
-      }
-};
+  const MyDietplan = async () => {
+    if (bmr === "") {
+      toast.error('Please enter all fields.');
+      return;
+    }
+
+    try {
+      const response = await axios.get(`${API}/diet/shortby`, {
+        params: {
+          dietpref: dietpref,
+          activity: activity
+        }
+      });
+
+      setResult(response.data); // Assuming the API returns the result in the response data
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      toast.error('Error fetching data. Please try again later.');
+    }
+  };
 
   return (
     <div className="main-container">
       <h1>Dietplan</h1>
-      
-      
       <div className='grid-box'>
-      <div>
-        <label>
-          BMR:
-          
-        </label>
-        <input
+        <div>
+          <label>BMR:</label>
+          <input
             type="number"
             value={bmr}
             onChange={(e) => setBmr(e.target.value)}
             placeholder='Enter your BMR'
           />
-      </div>
-      
-     
-      <div>
-      <label>Activty Level:</label>
-      <select value={activity} onChange={(e) => setActivity(e.target.value)}>
-            <option value="sedentary">Sedentary</option>
-            <option value="mactive">Moderately Active</option>
-            <option value="hactive">Highly Active</option>
-          </select>
-      </div>
+        </div>
 
-      <div>
-      <label>Dietary Preference:</label>
-      <select value={dietpref} onChange={(e) => setDietpref(e.target.value)}>
+        <div>
+          <label>Activty Level:</label>
+          <select value={activity} onChange={(e) => setActivity(e.target.value)}>
+            <option value="sedentary">Sedentary</option>
+            <option value="moderately">Moderately Active</option>
+            <option value="highly">Highly Active</option>
+          </select>
+        </div>
+
+        <div>
+          <label>Dietary Preference:</label>
+          <select value={dietpref} onChange={(e) => setDietpref(e.target.value)}>
             <option value="veg">Vegetarian</option>
             <option value="nonveg">Non Vegetarian</option>
           </select>
+        </div>
       </div>
-
-    
-
-      </div>
+      
       <button onClick={MyDietplan} className='mybtt'>Generate Dietplan</button>
       
       {result !== null && (
         <div className="result">
           <h2>Result:</h2>
           
+          <div dangerouslySetInnerHTML={{ __html: result[0].yourdiet }}/>
+            
+          
         </div>
       )}
-        
     </div>
   );
 };
